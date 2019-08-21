13d1424c18bfffe11aafa365568310fa72205386
Oliver Steele <steele@osteele.com>
2019-08-21 14:56:31 +0800

    Update home dotfiles

--- macos/install.sh
+++ macos/install.sh
@@ -41,9 +41,6 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 # Enable AirDrop over Ethernet and on unsupported Macs running Lion
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
-# Show the ~/Library folder
-chflags nohidden ~/Library
-
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
