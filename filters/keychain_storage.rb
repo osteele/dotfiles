0378d5423da77f92bd69dcfe44a118efb02d69a3
Oliver Steele <steele@osteele.com>
2020-05-22 12:45:22 -0400

    rework secrets filter

--- filters/keychain_storage.rb
+++ filters/keychain_storage.rb
@@ -9,7 +9,7 @@ class SecretsMap
   def for_path(path)
     return @map.keep_if do |key, v|
       key = "**/#{key}" unless key[0] == '/'
-      File.fnmatch(key, FNAME)
+      File.fnmatch(key, path)
     end.values.inject(&:merge)
   end
 end
@@ -18,25 +18,32 @@ module Secrets
   class KeychainStore
     SECURITY_CMD = '/usr/bin/security'
 
-    def _keys_for(name, key)
-      return ['-a', ENV['USER'], '-c', 'gitf', '-C', 'gitf', '-D', 'git filter secret', '-l', key]
+    def _args_for(name)
+      return [
+        '-a', ENV['USER'],
+        '-c', 'gitf',
+        '-C', 'gitf',
+        '-D', 'git filter secret',
+        '-l', name,
+        # FIXME remove reference to FNAME
+        '-s', "#{REPO_NAME}/#{FNAME}/#{name}"
+      ]
     end
 
-    def find(name, key)
+    def find(name)
       command = Shellwords.join([
         SECURITY_CMD, 'find-generic-password', '-w'] +
-        self._keys_for(name, key))
+        self._args_for(name))
       saved_secret = `#{command} 2>&1`.chomp
       return saved_secret if $?.exitstatus == 0
       return nil
     end
 
-    def update(name, key, password)
+    def update(name, password)
       command = Shellwords.join([
         SECURITY_CMD, 'add-generic-password',
-        '-s', "#{REPO_NAME}/#{FNAME}/#{name}",
         '-w', password,
-        '-U'] + self._keys_for(name, key))
+        '-U'] + self._args_for(name))
       `#{command}`
       exit 1 unless $?.exitstatus == 0
     end
